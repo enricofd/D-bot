65ca16a26c4152121148f802502f98be74a9d328
Enrico <enrico.francesco.damiani@gmail.com>
2023-03-26 12:44:25 -0300

    fix fix fix

--- core/local_search.py
+++ core/local_search.py
@@ -3,29 +3,34 @@ import re
 import json
 
 
-def search(query: str) -> list:
+def local_search(query: str) -> list:
 
     lemmatizer = WordNetLemmatizer()
 
-    with open('data/data.json', 'r') as f:
+    with open("data/data.json", "r") as f:
         data = json.load(f)
 
-    with open('data/title_url.json', 'r') as f:
+    with open("data/title_url.json", "r") as f:
         title_url = json.load(f)
 
-    
     result = dict()
-    words = [lemmatizer.lemmatize(word) for word in re.findall('\w+', query)]
-    
+    words = [lemmatizer.lemmatize(word) for word in re.findall("\w+", query)]
+
     for word in words:
-        
+
         if word in data.keys():
             for doc in data[word].keys():
-                
+
                 if doc not in result.keys():
                     result[doc] = data[word][doc]
-                    
+
                 else:
                     result[doc] += data[word][doc]
 
-    return [f"{title}: {title_url[title]}" for title in sorted(result)[:3]]
+    ordered_result = dict(
+        sorted(result.items(), key=lambda item: item[1], reverse=True)
+    )
+
+    return {
+        title: title_url[title] for title in list(ordered_result.keys())[:3]
+    }
---
+++ core/local_similarity_search.py
@@ -0,0 +1,57 @@
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+import re
+import json
+
+
+def local_similarity_search(query: str) -> list:
+
+    lemmatizer = WordNetLemmatizer()
+
+    with open("data/data.json", "r") as f:
+        data = json.load(f)
+
+    with open("data/title_url.json", "r") as f:
+        title_url = json.load(f)
+
+    result = dict()
+    words = [lemmatizer.lemmatize(word) for word in re.findall("\w+", query)]
+
+    for word in words:
+
+        max_word_similarity = 0
+        max_data_word_similarity = ""
+
+        for data_word in data.keys():
+            try:
+                syns1 = wordnet.synsets(word)[0]
+                syns2 = wordnet.synsets(data_word)[0]
+
+                path_similarity = syns1.path_similarity(syns2)
+                wup_similarity = syns1.wup_similarity(syns2)
+
+                total_similarity = (path_similarity + wup_similarity) / 2
+
+                if total_similarity > max_word_similarity:
+                    max_word_similarity = total_similarity
+                    max_data_word_similarity = data_word
+
+            except:
+                pass
+
+        if max_word_similarity > 0:
+            for doc in data[max_data_word_similarity].keys():
+
+                if doc not in result.keys():
+                    result[doc] = data[word][doc]
+
+                else:
+                    result[doc] += data[word][doc]
+
+    ordered_result = dict(
+        sorted(result.items(), key=lambda item: item[1], reverse=True)
+    )
+
+    return {
+        title: title_url[title] for title in list(ordered_result.keys())[:3]
+    }
